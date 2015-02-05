fa763c99e71e3a5f1271565853648de357969719
yuljin <yuljin3@naver.com>
2015-02-05 14:46:38 +0900

    no message

--- src/main/java/com/coreos/yuljin/HomeController.java
+++ src/main/java/com/coreos/yuljin/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) throws UnknownHostException {
-		logger.info("Welcome home!!a The client locale is {}.", locale);
+		logger.info("Welcome home!!ab The client locale is {}.", locale);
 		
 		InetAddress inetAddress = InetAddress.getLocalHost();
 		String hostName = inetAddress.getHostName();
